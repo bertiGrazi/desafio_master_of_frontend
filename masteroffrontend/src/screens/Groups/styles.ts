b47718ebcec3b6b0746529dd6bbfd4b8d17f3953
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-06 10:10:52 -0300

    using theme colors

--- masteroffrontend/src/screens/Groups/styles.ts
+++ masteroffrontend/src/screens/Groups/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native';
 
+type ThemeProp = { theme: any };
+
 export const Container = styled.View`
     flex: 1;
-    background-color: #2196F3;
-    align-items: center;
-    justify-content: center;
+    background-color: $({ theme }: ThemeProp) => theme.COLORS.GRAY_600;
+    padding: 24px;
 `;
 
 export const Title = styled.Text`
-    color: #FFFFF;
-    font-size: 32px;
+
 `
