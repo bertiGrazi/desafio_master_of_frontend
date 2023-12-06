9bab74e9d4719013a41ab3f123af072a6ac90e62
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-06 09:24:00 -0300

    config styled components

--- masteroffrontend/src/screens/Groups/index.tsx
+++ masteroffrontend/src/screens/Groups/index.tsx
@@ -1,19 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Eae!</Text>
-    </View>
+    <Container>
+      <Title>
+        Groups
+      </Title>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2196F3',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#003785'
-  },
-});
