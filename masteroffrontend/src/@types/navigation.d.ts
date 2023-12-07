cf764c64c8c327238a4d2d58d989715ca7d4268b
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-07 15:45:50 -0300

    route typing

---
+++ masteroffrontend/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RooParamList {
+      groups: undefined;
+      newCar: undefined;
+      details: {
+        group: string;
+      }
+    }
+  }
+}
