4810743083cbc76059ddba1089e545019fdb72d1
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-11 13:36:41 -0300

    handling exceptions

--- masteroffrontend/src/screens/NewCar/index.tsx
+++ masteroffrontend/src/screens/NewCar/index.tsx
@@ -1,14 +1,17 @@
-import { groupCreate } from "@storage/group/groupCreate";
-
 import { useState } from "react";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { Alert } from "react-native";
+
 
 
 export function NewCar() {
@@ -18,10 +21,18 @@ export function NewCar() {
   
   async function handleNewCar() {
     try {
+      if(group.trim().length === 0) {
+        return Alert.alert('Ops, algo deu errado', 'Informe o nome da marca do carro')
+      }
       await groupCreate(group)
       navigation.navigate('details', { group })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AppError) {
+        Alert.alert('Novo Anúncio', err.message)
+      } else {
+        Alert.alert('Novo Anúncio', 'Não foi possível criar um novo anúncio.')
+        console.log(err)
+      }
     }
   }
 
