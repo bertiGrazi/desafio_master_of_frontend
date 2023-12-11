e1de30e37d3b23c2c7e594339e34129eb8a196b0
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-11 13:19:33 -0300

    creating the persistent errors class

--- masteroffrontend/src/storage/group/groupCreate.ts
+++ masteroffrontend/src/storage/group/groupCreate.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
 
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 
@@ -7,6 +8,12 @@ import { groupsGetAll } from "./groupsGetAll";
 export async function groupCreate(newGroupName: string) {
   try {
     const storageGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storageGroups.includes(newGroupName)
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe esse carro cadastrado');
+    }
 
     const storage = JSON.stringify([...storageGroups, newGroupName])
     
