7533e181fa9e6d15b4da60f6c5baf751b4c970a2
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-11 10:11:15 -0300

    set handleNewCar at create buttom from NewCar

--- masteroffrontend/src/screens/NewCar/index.tsx
+++ masteroffrontend/src/screens/NewCar/index.tsx
@@ -1,3 +1,6 @@
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
 import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -5,6 +8,12 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewCar() {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  
+  function handleNewCar() {
+    navigation.navigate('details', { group: 'Fiat Argo'})
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,7 +28,7 @@ export function NewCar() {
         <Input placeholder="Ano de fabricação"/>
         <Input placeholder="Valor"/>
         <Input placeholder="Cidade"/> */}
-        <Button title="Criar" type="SECONDARY" style={{ marginTop: 20}}        
+        <Button title="Criar" type="SECONDARY" style={{ marginTop: 20}} onPress={handleNewCar}       
         />
       </Content>
     </Container>
