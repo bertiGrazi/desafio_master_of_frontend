3e488f9aeb39465519bc0a52cb0804a5e0faac63
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-12 13:21:12 -0300

    create signIn and router screan

--- masteroffrontend/src/routes/app.routes.tsx
+++ masteroffrontend/src/routes/app.routes.tsx
@@ -4,12 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Groups } from '@screens/Groups';
 import { Details } from '@screens/Details';
 import { NewCar } from '@screens/NewCar'
+import { SignIn } from '@screens/Login/SignIn'
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator initialRouteName='groups' screenOptions={{ headerShown: false}}>
+    <Navigator initialRouteName='signIn' screenOptions={{ headerShown: false}}>
       <Screen 
         name="groups" 
         component={Groups}
@@ -21,6 +22,10 @@ export function AppRoutes() {
       <Screen 
         name="details" 
         component={Details} 
+      />
+      <Screen 
+        name="signIn" 
+        component={SignIn} 
       />
     </Navigator>
   );
