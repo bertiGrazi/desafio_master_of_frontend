4907cacd5b25cf8c4cb7517133a52f8e6fc2d26c
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-12 13:35:05 -0300

    set component at signIn scream

--- masteroffrontend/src/screens/Login/SignIn/index.tsx
+++ masteroffrontend/src/screens/Login/SignIn/index.tsx
@@ -1,10 +1,41 @@
 import { Container, Content } from "./styles";
 
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
 export function SignIn() {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  async function handleSignIn() {
+    navigation.navigate('groups')
+  }
+
   return (
     <Container>
       <Content>
-        
+        <Highlight 
+          title="Acesse a sua conta"
+        />
+        <Input 
+          placeholder="E-mail"
+        />
+        <Input
+           placeholder="Senha"
+        />
+        <Button 
+          title="Acessar"
+          style={{ marginTop: 20}}
+          onPress={handleSignIn}
+        />
+
+        <Highlight
+          title="" 
+          subtitle="Ainda não tem acesso?"
+        />    
       </Content>
     </Container>
   )
